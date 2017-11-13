30f6c36e99afe8fbe1c40725bbb2603658742cf2
What Bird Is That? <95787+whatbirdisthat@users.noreply.github.com>
2017-11-13 23:51:37 +0000

    add all sorts of extra bits and pieces because my x11 had the security settings mysteriously switched off again :facepalm:

--- src/image-firefox.bash
+++ src/image-firefox.bash
@@ -2,5 +2,13 @@
 
 firefox() {
   ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
-  docker run --rm -d --name firefox -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix jess/firefox
+  xhost + $ip
+  docker run --rm -d --name firefox \
+    --net host \
+    -e DISPLAY=$ip:0 \
+    -e XAUTHORITY=/tmp/xauth \
+    -v ${HOME}/.Xauthority:/tmp/xauth \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v #CONTAINER_FIREFOX_LOCATION#/.mozilla:/root/.mozilla \
+    jess/firefox
 }
